bced1ac7eb2e836d37928be5a24db9b271a74d2c
gtwatt1 <gtwatt1001@gmail.com>
2020-09-25 04:53:29 +0100

    completed ui implementation in storyboard

---
+++ WeatherVIP/feature/forecast/WeatherViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherViewController.swift
+//  Weather-MVP
+//
+//  Created by Godwin Olorunshola on 16/12/2019.
+//  Copyright © 2019 Godwin Olorunshola. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var minTemperatureLabel: UILabel!
+    @IBOutlet weak var maxTemperatureLabel: UILabel!
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var moreCurrentDayTemperatureDetailsLabel: UIStackView!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
